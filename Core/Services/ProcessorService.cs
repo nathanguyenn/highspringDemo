b4cc7faf857539d3483433019d63ec33c8f71e38
nathanguyenn <nathanguyenn@gmail.com>
2025-05-27 16:28:26 -0500

    initial commit

---
+++ Core/Services/ProcessorService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication1.Core.Contracts;
+
+namespace WebApplication1.Core.Services
+{
+    public class ProcessorService : IProcessorService
+    {
+        public ProcessorService() { }
+        public Task<string>ProcessMessage(string id)
+        {
+            return Task.FromResult("This is result for id: aaaa");
+        }
+    }
+}
